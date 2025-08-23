020b2c1095abc90fff58261991e0908aa7bb8612
Omar <61301120+OmarChura@users.noreply.github.com>
2025-08-23 19:41:54 -0300

    Loja Virtual
    Asp.Net MVC

---
+++ LojaVirtual/LojaVirtual/Controllers/ProdutoController.cs
@@ -0,0 +1,26 @@
+﻿using LojaVirtual.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LojaVirtual.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        public ActionResult Visualizar()
+        {
+            Produto produto = GetProduto();
+            
+            return View(produto);
+            //return  new ContentResult() { Content = "<h3>Produto Visualizar<h3>", ContentType = "text/html"};
+        }
+
+        private Produto GetProduto() { 
+            return new Produto()
+            {
+                Id = 1,
+                Nome = "xbos one",
+                Descricao = "joguar",
+                Valor  = 2000
+            };
+        }
+    }
+}
